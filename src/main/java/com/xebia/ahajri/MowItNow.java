1a1bf75cd432af2b6ed9fb5dddaa79a4d4210b9a
Anis HAJRI <anis.hajri@gmail.com>
2016-06-12 20:07:09 +0200

    Initial commit

--- src/main/java/com/xebia/ahajri/MowItNow.java
+++ src/main/java/com/xebia/ahajri/MowItNow.java
@@ -2,17 +2,27 @@ package com.xebia.ahajri;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 
 public class MowItNow {
 
-	private static final File input = new File("input.txt");
-
 	public static void main(String[] args) {
 		try {
-			System.out.println(FileUtils.readLines(input, Charset.defaultCharset()));
+			URL url = MowItNow.class.getClassLoader().getResource("input.txt");
+			File input = new File(url.getFile());
+			List<String> lines = FileUtils.readLines(input, Charset.defaultCharset());
+			//get area spaces
+			String[] posMax=lines.get(0).split(" ");
+			for (String s : posMax) {
+				System.out.println(s);
+			}
+			for (String line : lines) {
+				System.out.println(line);
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
--- src/main/java/com/xebia/ahajri/Mower.java
+++ src/main/java/com/xebia/ahajri/Mower.java
@@ -5,7 +5,18 @@ import java.util.List;
 
 public class Mower {
 	
-	private Position p;
+	private Position startPos;
 	private List<Position> forbiddenPositions=new ArrayList<>();
+	
+	public Mower(Position startPos, List<Position> forbiddenPositions) {
+		super();
+		this.startPos = startPos;
+		this.forbiddenPositions.addAll(forbiddenPositions);
+	}
+	
+	
 
+	public Position mow(String line){
+		return null;
+	}
 }
